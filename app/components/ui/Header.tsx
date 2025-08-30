93899ca535d350938c16829d1365e4c1e477b2fa
devakowakou <akowakou251@gmail.com>
2025-08-30 02:52:49 +0100

    update div header

--- app/components/ui/Header.tsx
+++ app/components/ui/Header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
             <div className='navbar  shadow-xl rounded-box p-2 md:px-10'>
                 <div className='navbar-start'>
                     <div className='dropdown'>
-                        <div tabIndex={0} role='button' className='btn btn-ghost lg:hidden'>
+                        <div tabIndex={0} role='button' className='btn btn-ghost lg:hidden' aria-label="Open navigation menu" title="Open navigation menu">
                             <svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-5' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
                                 <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M4 6h16M4 12h8m-8 6h16' />
                             </svg>
